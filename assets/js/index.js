8cad9c88f0a9fda469c070bdfbcb376e22108db0
victoriastruk <struk.victoria@gmail.com>
2024-10-23 22:28:24 +0300

    Refactoring

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,65 @@
 "use strict";
-fetch("https://fakestoreapi.com/products")
+
+const productsUrl = "https://fakestoreapi.com/products";
+fetch(productsUrl)
   .then((response) => response.json())
   .then((data) => generateCards(data))
   .catch((err) => console.log("err: ", err));
 
-function generateCards(data) {
+function generateCards(products) {
   const container = document.createElement("div");
   container.classList.add("container");
   document.body.append(container);
 
-  data.forEach((product) => {
-    const card = document.createElement("a");
-    card.setAttribute("href", "#");
-    card.classList.add("card");
+  products.forEach((product) => {
+    const card = createCard(product);
     container.append(card);
-
-    const rateBlock = document.createElement("div");
-    rateBlock.classList.add("rate");
-    card.append(rateBlock);
-
-    const rateValue = document.createElement("span");
-    rateValue.textContent = product.rating.rate;
-    rateBlock.append(rateValue);
-
-    const star = document.createElement("i");
-    star.classList.add("fa-solid");
-    star.classList.add("fa-star");
-    star.classList.add("rate-icon");
-    rateBlock.append(star);
-
-    const imgCard = document.createElement("img");
-    imgCard.classList.add("card-img");
-    imgCard.setAttribute("alt", `${product.title}`);
-    imgCard.setAttribute("src", `${product.image}`);
-    card.append(imgCard);
-
-    const titleCard = document.createElement("h2");
-    titleCard.classList.add("card-title");
-    titleCard.textContent = product.title;
-    card.append(titleCard);
-
-    const price = document.createElement("div");
-    price.classList.add("price");
-    price.textContent = `$${product.price}`;
-    card.append(price);
   });
+}
+
+function createCard({ rating, title, image, price: productPrice }) {
+  const card = document.createElement("a");
+  card.href = "#";
+  card.classList.add("card");
+  const rateBlock = createRateBlock(rating);
+  const imgCard = createImage(image, title);
+  const titleCard = createTitle(title);
+  const price = createPriceBlock(productPrice);
+  card.append(rateBlock, imgCard, titleCard, price);
+  return card;
+}
+
+function createRateBlock(rating) {
+  const rateBlock = document.createElement("div");
+  rateBlock.classList.add("rate");
+  const rateValue = document.createElement("span");
+  rateValue.textContent = rating.rate;
+
+  const star = document.createElement("i");
+  star.classList.add("fa-solid", "fa-star", "rate-icon");
+  rateBlock.append(rateValue, star);
+
+  return rateBlock;
+}
+
+function createImage(src, alt) {
+  const imgCard = document.createElement("img");
+  imgCard.classList.add("card-img");
+  imgCard.alt = alt;
+  imgCard.src = src;
+  return imgCard;
+}
+
+function createTitle(title) {
+  const titleCard = document.createElement("h2");
+  titleCard.classList.add("card-title");
+  titleCard.textContent = title;
+  return titleCard;
+}
+
+function createPriceBlock(price) {
+  const priceBlock = document.createElement("div");
+  priceBlock.classList.add("price");
+  priceBlock.textContent = `$${price}`;
+  return priceBlock;
 }
